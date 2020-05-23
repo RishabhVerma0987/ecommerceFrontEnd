0eb21b120d7317e93f5d5d9e5aa08faa9974de01
rishabh verma <vermarishabh0987@gmail.com>
2020-05-23 23:56:40 +0530

    homepage basic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Background from "./components/background/background";
+import Homepage from "./components/homepage/homepage";
 import "./App.scss";
 function App() {
   return (
     <div className="App">
       <Background image={"banner"} bgColor={"#1a4c31"} />
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./homepage.scss";
+function Homepage() {
+  return (
+    <div className="homepageBanner">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+export default Homepage;
