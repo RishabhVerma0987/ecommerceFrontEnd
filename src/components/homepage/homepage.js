f49d22e80cf8710d3a3a5567f9883f6c459bb44a
rishabh verma <vermarishabh0987@gmail.com>
2020-05-24 00:33:11 +0530

    homepage left content

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -3,7 +3,19 @@ import "./homepage.scss";
 function Homepage() {
   return (
     <div className="homepageBanner">
-      <h1>hello world</h1>
+      <div className="content">
+        <h1>
+          Browse Best Games <br />
+          Online
+        </h1>
+        <p>at very affordable prices!!</p>
+        <button>Browse All</button>
+      </div>
+      <div className="upcommingGames">
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
+      </div>
     </div>
   );
 }
