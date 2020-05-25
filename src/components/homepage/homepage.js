d67f38bf4f5f3ca0fe6cfd17f0230d80954ed532
rishabh verma <vermarishabh0987@gmail.com>
2020-05-25 20:39:18 +0530

    animation on enter and leave done

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,16 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { topGamesBanner } from "../../store/actions";
 import "./homepage.scss";
 function Homepage() {
   const dispatch = useDispatch();
   const stateTopGames = useSelector((state) => state.bannerTopGamesReducer);
-
+  const [scale, setScale] = useState([1, 0.8, 0.8]);
   useEffect(() => {
     dispatch(topGamesBanner());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  console.log(stateTopGames);
+
+  const changeScale = (id) => {
+    let temp = scale;
+    temp[id] = 1;
+    for (let i = 0; i < scale.length; i++) {
+      if (i !== id) {
+        temp[i] = 0.8;
+      }
+    }
+    setScale([...temp]);
+  };
   return (
     <div className="homepageBanner">
       <div className="content">
@@ -21,7 +31,6 @@ function Homepage() {
         <p>at very affordable prices!!</p>
         <button>Browse All</button>
       </div>
-
       {stateTopGames ? (
         <div className="upcommingGames">
           {stateTopGames.map((game) => {
@@ -30,8 +39,12 @@ function Homepage() {
                 className="box"
                 key={game.id}
                 style={{
-                  transform: `scale(${game.scale})`,
-                  opacity: game.scale === 1 ? 1 : 0.5,
+                  transform: `scale(${scale[game.id - 1]})`,
+                  opacity: scale[game.id - 1] === 1 ? 1 : 0.5,
+                }}
+                onMouseEnter={() => changeScale(game.id - 1)}
+                onMouseLeave={() => {
+                  setScale([1, 0.8, 0.8]);
                 }}
               >
                 <div className="nameBox">
