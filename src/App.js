d704a1737747d9b0f7df649bbe23a97777d3729b
rishabh verma <vermarishabh0987@gmail.com>
2020-05-26 23:46:06 +0530

    lastest page initialized

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Background from "./components/background/background";
 import Homepage from "./components/homepage/homepage";
 import Trending from "./components/trending/trending";
+import Latest from "./components/latest/latest";
 import "./App.scss";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Background image={"banner"} bgColor={"#1a4c31"} />
       <Homepage />
       <Trending />
+      <Latest />
     </div>
   );
 }
---
+++ src/components/latest/latest.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./latest.scss";
+function Latest() {
+  return (
+    <React.Fragment>
+      <div className="latest">
+        <h1>hello</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+export default Latest;
