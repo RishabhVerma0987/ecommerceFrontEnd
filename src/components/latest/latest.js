07a45cfa57b2664d553383954544472632e6635c
rishabh verma <vermarishabh0987@gmail.com>
2020-05-27 23:58:02 +0530

    again fixed changes to background banner

--- src/components/latest/latest.js
+++ src/components/latest/latest.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./latest.scss";
+import Background from "../background/background";
 function Latest() {
   return (
     <React.Fragment>
       <div className="latest">
+        <Background image={"cyberpunk"} bgColor={"#1f2227"} />
         <h1>hello</h1>
       </div>
     </React.Fragment>
