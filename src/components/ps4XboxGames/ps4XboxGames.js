fb40f31d05f0722fa2e9276528bc843593b09ef1
rishabh verma <vermarishabh0987@gmail.com>
2020-06-14 20:27:24 +0530

    basic structure V2 done

--- src/components/ps4XboxGames/ps4XboxGames.js
+++ src/components/ps4XboxGames/ps4XboxGames.js
@@ -1,4 +1,6 @@
 import React from "react";
+import CartSVG from "../resuables/cartSVG";
+import WishListSVG from "../resuables/wishlistSVG";
 import "./ps4XboxGames.scss";
 function Ps4XboxGames({ platform }) {
   return (
@@ -6,12 +8,118 @@ function Ps4XboxGames({ platform }) {
       <div className="container">
         <h1>{platform} Games</h1>
         <div className="content">
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
-          <div className="card"></div>
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy">
+                <div className="title">
+                  <h5>Cyberpunk 2077</h5>
+                </div>
+                <div className="priceRating">
+                  <div className="price">
+                    <p>$ 25.00 /- </p>
+                  </div>
+                  <div className="rating">
+                    <p>3.5 S</p>
+                  </div>
+                </div>
+              </div>
+              <div className="cartWish">
+                <WishListSVG />
+                <CartSVG />
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy"></div>
+              <div className="cartWish"></div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy"></div>
+              <div className="cartWish"></div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy"></div>
+              <div className="cartWish"></div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy"></div>
+              <div className="cartWish"></div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy"></div>
+              <div className="cartWish"></div>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy"></div>
+              <div className="cartWish"></div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="dp">
+              <img
+                src={require("../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              />
+            </div>
+            <div className="info">
+              <div className="buy"></div>
+              <div className="cartWish"></div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
