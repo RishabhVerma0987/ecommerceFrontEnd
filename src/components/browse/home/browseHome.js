23df95139b363a6b95d7448567076c06877a453f
rishabh verma <vermarishabh0987@gmail.com>
2020-06-16 00:57:51 +0530

    filter structure done

--- src/components/browse/home/browseHome.js
+++ src/components/browse/home/browseHome.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./browseHome.scss";
 import Navbar from "../../main/navbar/navbar";
+import AnimateHeight from "react-animate-height";
 function BrowseHome() {
   const category = [
     {
@@ -21,7 +22,35 @@ function BrowseHome() {
       ],
       id: 2,
     },
+    {
+      title: "Platform",
+      values: [
+        { id: 1, name: "PS4" },
+        { id: 2, name: "XBOX" },
+        { id: 3, name: "PC" },
+      ],
+      id: 3,
+    },
+    {
+      title: "Genre",
+      values: [
+        { id: 1, name: "RPG" },
+        { id: 2, name: "Racing" },
+        { id: 3, name: "Horror" },
+      ],
+      id: 4,
+    },
   ];
+
+  const [height, setHeight] = useState(0);
+
+  const toggle = () => {
+    if (height === 0) {
+      setHeight("auto");
+    } else {
+      setHeight(0);
+    }
+  };
 
   return (
     <React.Fragment>
@@ -54,6 +83,51 @@ function BrowseHome() {
               );
             })}
           </div>
+        </div>
+        <div className="filter-mobile">
+          <div className="controls">
+            <div className="filt" onClick={toggle}>
+              <p>filter</p>
+            </div>
+            <div className="sort">
+              <p>sort</p>
+            </div>
+          </div>
+          <AnimateHeight
+            duration={500}
+            height={height} // see props documentation below
+            className="menu"
+          >
+            <div className="box">
+              {category.map((i) => {
+                return (
+                  <div className={i.title}>
+                    <h5>{i.title}</h5>
+                    <ul>
+                      {i.values.map((j) => {
+                        return (
+                          <li>
+                            <input
+                              class="styled-checkbox"
+                              id={`styled-checkbox-${i.id}-${j.id}`}
+                              type="checkbox"
+                              value={`value${j.id}`}
+                            />
+                            <label for={`styled-checkbox-${i.id}-${j.id}`}>
+                              {j.name}
+                            </label>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="apply">
+              <button onClick={toggle}>Apply filter</button>
+            </div>
+          </AnimateHeight>
         </div>
         <div className="showcase"></div>
       </div>
