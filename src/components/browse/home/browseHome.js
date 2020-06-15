9a84f4a95bd72f2962bce83b52848f1f1aa2b99c
rishabh verma <vermarishabh0987@gmail.com>
2020-06-15 15:04:19 +0530

    filter structure done

--- src/components/browse/home/browseHome.js
+++ src/components/browse/home/browseHome.js
@@ -2,40 +2,57 @@ import React from "react";
 import "./browseHome.scss";
 import Navbar from "../../main/navbar/navbar";
 function BrowseHome() {
+  const category = [
+    {
+      title: "Platform",
+      values: [
+        { id: 1, name: "PS4" },
+        { id: 2, name: "XBOX" },
+        { id: 3, name: "PC" },
+      ],
+      id: 1,
+    },
+    {
+      title: "Genre",
+      values: [
+        { id: 1, name: "RPG" },
+        { id: 2, name: "Racing" },
+        { id: 3, name: "Horror" },
+      ],
+      id: 2,
+    },
+  ];
+
   return (
     <React.Fragment>
       <Navbar color={"#29282f"} />
       <div className="browseHome">
         <div className="filter">
           <div className="box">
-            <div className="platform">
-              <h5>Platform</h5>
-              <ul>
-                <li>
-                  <input
-                    class="styled-checkbox"
-                    id="styled-checkbox-1-1"
-                    type="checkbox"
-                    value="value1"
-                  />
-                  <label for="styled-checkbox-1-1">Checkbox</label>
-                </li>
-              </ul>
-            </div>
-            <div className="genre">
-              <h5>Genre</h5>
-              <ul>
-                <li>
-                  <input
-                    class="styled-checkbox"
-                    id="styled-checkbox-2-1"
-                    type="checkbox"
-                    value="value1"
-                  />
-                  <label for="styled-checkbox-2-1">Checkbox</label>
-                </li>
-              </ul>
-            </div>
+            {category.map((i) => {
+              return (
+                <div className={i.title}>
+                  <h5>{i.title}</h5>
+                  <ul>
+                    {i.values.map((j) => {
+                      return (
+                        <li>
+                          <input
+                            class="styled-checkbox"
+                            id={`styled-checkbox-${i.id}-${j.id}`}
+                            type="checkbox"
+                            value={`value${j.id}`}
+                          />
+                          <label for={`styled-checkbox-${i.id}-${j.id}`}>
+                            {j.name}
+                          </label>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="showcase"></div>
