51e69b877e794a808fabfc8b5d9127d8c531ebb8
rishabh verma <vermarishabh0987@gmail.com>
2020-06-19 13:45:27 +0530

    wishlist done

--- src/components/user/wishlist/wishlist.js
+++ src/components/user/wishlist/wishlist.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./wishlist.scss";
+import Card from "../../main/resuables/card";
+import { ps4Games } from "../../../store/actions/index";
 function WishList() {
+  const dispatch = useDispatch();
+  const games = useSelector((state) => state.ps4GamesReducer);
+  useEffect(() => {
+    dispatch(ps4Games());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="wishlistCompo">
-      <h1>whishlist</h1>
+      <div className="workingArea">
+        <div className="container">
+          <div className="content">
+            {games?.map((i) => {
+              return (
+                <Card
+                  gameName={i.game_name}
+                  imageName={i.image_name}
+                  price={i.price}
+                  rating={i.rating}
+                  key={i.id}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
