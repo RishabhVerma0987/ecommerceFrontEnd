7a1228adaecb788646c87068bdc14e20c861d94e
rishabh verma <vermarishabh0987@gmail.com>
2020-06-19 13:25:00 +0530

    my games done

--- src/components/user/mygames/mygame.js
+++ src/components/user/mygames/mygame.js
@@ -3,7 +3,23 @@ import "./mygames.scss";
 function MyGames() {
   return (
     <div className="mygames">
-      <h1>my games</h1>
+      <div className="cart-contents">
+        <div className="item">
+          <div className="left">
+            <div className="display-photo">
+              <img
+                src={require("../../../assets/cyberpunk.jpg")}
+                alt="cyberpunk"
+              ></img>
+            </div>
+            <div className="information">
+              <h4>Cyberpunk</h4>
+              <p>3.5 , By CDKproject</p>
+            </div>
+          </div>
+          <div className="right">$ 35.00 /-</div>
+        </div>
+      </div>
     </div>
   );
 }
