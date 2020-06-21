b14acf291e71f2d76815c9f82de2d0abfd7fa15c
rishabh verma <vermarishabh0987@gmail.com>
2020-06-21 14:57:44 +0530

    stable

--- src/store/actions/browse.js
+++ src/store/actions/browse.js
@@ -6,7 +6,20 @@ import { url } from "./index";
  * @param dispatch : allGames()
  */
 
-export const fetchAllGames = () => {
+/**
+ *
+ * @param query = {
+ *
+ *  platform:[PS4,XBOX,PX],
+ *  genre:[RPG,...]
+ *  price:[under 1000 , ...] //!needs further transformation to [{ sign:"lt" , value:1000 }]
+ *  pegi:[pegi 18+ , pegi 13+] //!needs futher transform [{ sign:"gt" , value:18 }]
+ *  comapany:[Ubisoft , ...]
+ *  user rating:[1-2,2-3,...] //!just add [lt] 2 , [gt] 3
+ * }
+ */
+
+export const fetchAllGames = (query) => {
   return function (dispatch) {
     return axios
       .get(`${url}/product`)
